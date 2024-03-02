eef6f64e0c05bcd2c6ccd0870102e1e3c4fc5510
Sadika Rahman <sadikar03@gmail.com>
2024-03-02 20:45:10 +0000

    6th commit:Finished Home Page design

---
+++ src/components/GameSlide.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { SwiperSlide } from 'swiper/react';
+
+
+function GameSlide({game, index, activeVideoIndex, handleToggleVideo}) {
+  return (
+    <SwiperSlide>
+    <div className="gameSlider">
+      <img src={game.img} alt="Game Image" />
+      <div className={`video ${activeVideoIndex === index ? 'active' : ''}`}>
+        {activeVideoIndex === index && (
+          <iframe
+            width="1280"
+            height="720"
+            src={game.trailer}
+            title={game.title}
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+            allowFullScreen
+          ></iframe>
+        )}
+      </div>
+      <div className="content">
+        <h2>{game.title}</h2>
+        <p>{game.description}</p>
+        <div className="buttons">
+          <a href="#" className="orderBtn">
+            Order Now
+          </a>
+          <a href="#" className="playBtn" onClick={() => handleToggleVideo(index)}>
+            {activeVideoIndex === index ? (
+              <span className="pause">
+                <i className="bi bi-pause-fill"></i>
+              </span>
+            ) : (
+              <span className="play">
+                <i className="bi bi-play-fill"></i>
+              </span>
+            )}
+          </a>
+        </div>
+      </div>
+    </div>
+  </SwiperSlide>
+  );
+}
+
+export default GameSlide;
--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -5,6 +5,7 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import './gameSwiper.css';
 import { EffectCoverflow, Navigation, Autoplay } from 'swiper/modules';
+import GameSlide from './GameSlide';
 
 function GameSwiper({ games }) {
   const [activeVideoIndex, setActiveVideoIndex] = useState(null);
@@ -33,39 +34,12 @@ function GameSwiper({ games }) {
     >
       {games.map((game, index) => (
         <SwiperSlide key={game._id}>
-          <div className="gameSlider">
-            <img src={game.img} alt="Game Image" />
-            <div className={`video ${activeVideoIndex === index ? 'active' : ''}`}>
-              <iframe
-                width="1280"
-                height="720" 
-                src={activeVideoIndex === index ? game.trailer : ''}
-                title={game.title}
-                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-                allowFullScreen
-              ></iframe>
-            </div>
-            <div className="content">
-              <h2>{game.title}</h2>
-              <p>{game.description}</p>
-              <div className="buttons">
-                <a href="#" className="orderBtn">
-                  Order Now
-                </a>
-                <a href="#" className="playBtn" onClick={() => handleToggleVideo(index)}>
-                  {activeVideoIndex === index ? (
-                    <span className="pause">
-                      <i className="bi bi-pause-fill"></i>
-                    </span>
-                  ) : (
-                    <span className="play">
-                      <i className="bi bi-play-fill"></i>
-                    </span>
-                  )}
-                </a>
-              </div>
-            </div>
-          </div>
+          <GameSlide
+            game={game}
+            index={index}
+            activeVideoIndex={activeVideoIndex}
+            handleToggleVideo={handleToggleVideo}
+          />
         </SwiperSlide>
       ))}
     </Swiper>
