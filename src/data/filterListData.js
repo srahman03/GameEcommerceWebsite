1eaa6d496ccc04ef0d4281079f06e01d06a20fe8
Sadika Rahman <sadikar03@gmail.com>
2024-03-04 19:54:30 +0000

    9th commit: category section done

---
+++ src/data/filterListData.js
@@ -0,0 +1,38 @@
+const filterListData = [
+    {
+        _id: 1,
+        name: 'All',
+        active: true,
+    },
+    {
+        _id: 2,
+        name: 'Action',
+        active: false,
+    },
+    {
+        id: 3,
+        name: 'MOBA',
+        active: false,
+    },
+    {
+        _id: 4,
+        name: 'Battle',
+        active: false,
+    },
+    {
+        _id: 5,
+        name: 'Racing',
+        active: false,
+    },
+    {
+        _id: 6,
+        name: 'Fighting', 
+        active: false,
+    },
+    {
+        _id: 7,
+        name: 'RPG', 
+        active: false,
+    },
+];
+export default filterListData;
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './categories.css'
+import filterListData from '../data/filterListData'
+import GameCard from '../components/GameCard';
 
-function Categories({games, reference}) {
+
+function Categories({ games, reference }) {
+
+  const [data, setData] = useState(games);
+
+  const [filters, setFilters] = useState(filterListData)
+
+  const handleFilterGames = (category) => {
+    setFilters(filters.map(filter => {
+      filter.active = false;
+      if (filter.name === category) {
+        filter.active = true
+      }
+      return filter;
+    }));
+
+
+    if (category === 'All') {
+      setData(games);
+      return;
+    }
+    setData(games.filter(game => game.category === category));
+  };
+
+  const [text, setText] = useState('');
+  const handleSearchGames = e => {
+    setData(games.filter(game => game.title.toLowerCase().includes(e.target.value.toLowerCase())));
+    setText(e.target.value);
+  }
+
   return (
     <section id="categories" className='categories' ref={reference}>
-        <h1>Categories</h1>
+      <div className="container-fluid mt-2">
+        <div className="row">
+          <div className="col-lg-8 d-flex align-items-center justify-content-start">
+            <ul className="filters">
+              {filters.map(filter => (
+                <li key={filter._id}
+                  className={`${filter.active ? 'active' : undefined}`}
+                  onClick={() => handleFilterGames(filter.name)}>
+                  {filter.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="col-lg-4 d-flex align-items-center justify-content-end">
+            <div className="search"><i className="bi bi-search"></i>
+              <input type="text"
+                name="search"
+                value={text}
+                placeholder='Search'
+                onChange={handleSearchGames} />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {data.map(game => (
+            <GameCard key={game._id} game={game} />
+          ))}
+        </div>
+      </div>
     </section>
   )
 }
