c9b3172cbeb03c03d5ca4da5ce2f26c677a83024
Sadika Rahman <sadikar03@gmail.com>
2024-04-17 18:03:18 +0100

    Made promotion games dynamic, filters all games to find discounted games and displays them on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from 'react';
+import GameSwiper from '../components/GameSwiper';
+import GameCard from '../components/GameCard';
 
-import './home.css'
-import GameSwiper from '../components/GameSwiper'
-import GameCard from '../components/GameCard'
-function Home({games, reference}) {
+function Home({ games, reference }) {
+  // Filter games that have a promotion
+  const gamesOnPromotion = games.filter(game => game.discount > 0);
+
   return (
-  <section id="home" className='home active' ref={reference}>
-    <div className="container-fluid">
-      <div className="row">
-        <GameSwiper games={games}/>
-      </div> 
-      <div className="row mb-4 mt-4" >
-        <div className="col-lg-6">
-          <h2 className="sectionTitle">Games on promotion</h2>
+    <section id="home" className='home active' ref={reference}>
+      <div className="container-fluid">
+        <div className="row">
+          <GameSwiper games={games}/>
+        </div> 
+        <div className="row mb-4 mt-4" >
+          <div className="col-lg-6">
+            <h2 className="sectionTitle">Games on promotion</h2>
+          </div>
         </div>
-        
-      </div>
-      <div className="row">
-        {
-          games.slice(0,4).map(game=>(
-            <GameCard key={game._id} game ={game}/>
+        <div className="row">
+          {gamesOnPromotion.slice(0, 15).map(game => (
+            <GameCard key={game._id} game={game} />
           ))}
+        </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Home
+export default Home;
